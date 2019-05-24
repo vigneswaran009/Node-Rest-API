9f5b687efbccf1d151242a1abf622e7de4033655
vigneswaran009 <50344478+vigneswaran009@users.noreply.github.com>
2019-05-24 08:42:08 +0530

    Add files via upload

---
+++ api/routes/products.js
@@ -0,0 +1,68 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+
+//Eccentric to know
+
+router.get('/', (req, res, next) => {
+
+    res.status(200).json({
+        message: 'Handling GET requests to /products'
+    });
+});
+
+
+
+router.post('/', (req, res, next) => {
+
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    };
+    res.status(200).json({
+        message: 'Handling POST requests to /products',
+        createdProduct: product
+    });
+});
+
+
+router.get('/:productId', (req, res, next)=>{
+
+    const id = req.params.productId;
+
+    if(id === 'special'){
+        res.status(200).json({
+            message: 'You discovered the Special ID',
+            id: id
+        })
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        })
+    }
+
+});
+
+
+
+
+router.patch('/:productId', (req, res, next)=>{
+
+    res.status(200).json({
+        message: 'Updated product!'
+    });
+});
+
+
+
+
+
+router.delete('/:productId', (req, res, next)=>{
+
+    res.status(200).json({
+        message: 'Deleted product!'
+    });
+});
+module.exports = router;
